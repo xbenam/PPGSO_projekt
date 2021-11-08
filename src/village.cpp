1224ab748358ebb033dc89d3fae705820d261746
xbenam <xbenam@gmail.com>
2021-11-08 21:17:32 +0100

    start

--- src/village.cpp
+++ src/village.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
 int main() {
-<<<<<<< HEAD
-    std::cout << "Skúšam niečo upraviť!" << std::endl;
-    std::cout << "Merge" << std::endl;
-=======
     std::cout << "Hello, World!" << std::endl;
->>>>>>> parent of b6935d9 (Mario sa hlási)
     return 0;
 }
