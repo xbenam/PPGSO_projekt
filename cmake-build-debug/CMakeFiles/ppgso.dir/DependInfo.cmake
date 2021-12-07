3a8dd7f16e926821d7b3dd22ac387a9e88c76878
xbenam <xbenam@gmail.com>
2021-12-07 17:41:53 +0100

    To main commit

--- cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake
@@ -1,24 +1,35 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/image.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/image_bmp.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_bmp.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/image_raw.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_raw.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/mesh.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/mesh.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/shader.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/shader.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/texture.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/texture.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/tiny_obj_loader.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/tiny_obj_loader.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/ppgso/window.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/window.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/ground.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/src/Village/ground.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/object.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/src/Village/object.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/scene.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/src/Village/scene.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLM_FORCE_RADIANS"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/image.cpp" "CMakeFiles/ppgso.dir/ppgso/image.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/image.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/image_bmp.cpp" "CMakeFiles/ppgso.dir/ppgso/image_bmp.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/image_bmp.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/image_raw.cpp" "CMakeFiles/ppgso.dir/ppgso/image_raw.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/image_raw.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/mesh.cpp" "CMakeFiles/ppgso.dir/ppgso/mesh.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/mesh.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/shader.cpp" "CMakeFiles/ppgso.dir/ppgso/shader.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/shader.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/texture.cpp" "CMakeFiles/ppgso.dir/ppgso/texture.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/texture.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/tiny_obj_loader.cpp" "CMakeFiles/ppgso.dir/ppgso/tiny_obj_loader.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/tiny_obj_loader.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/ppgso/window.cpp" "CMakeFiles/ppgso.dir/ppgso/window.cpp.o" "gcc" "CMakeFiles/ppgso.dir/ppgso/window.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/ground.cpp" "CMakeFiles/ppgso.dir/src/Village/ground.cpp.o" "gcc" "CMakeFiles/ppgso.dir/src/Village/ground.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/object.cpp" "CMakeFiles/ppgso.dir/src/Village/object.cpp.o" "gcc" "CMakeFiles/ppgso.dir/src/Village/object.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/scene.cpp" "CMakeFiles/ppgso.dir/src/Village/scene.cpp.o" "gcc" "CMakeFiles/ppgso.dir/src/Village/scene.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ppgso"
+  "../dependencies/include"
+  "../"
+  "."
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/village.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/village.dir/DependInfo.cmake
@@ -1,23 +1,34 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/ground.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/village.dir/src/Village/ground.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/object.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/village.dir/src/Village/object.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/scene.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/village.dir/src/Village/scene.cpp.obj"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/src/Village/village.cpp" "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/village.dir/src/Village/village.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLM_FORCE_RADIANS"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/ground.cpp" "CMakeFiles/village.dir/src/Village/ground.cpp.o" "gcc" "CMakeFiles/village.dir/src/Village/ground.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/object.cpp" "CMakeFiles/village.dir/src/Village/object.cpp.o" "gcc" "CMakeFiles/village.dir/src/Village/object.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/scene.cpp" "CMakeFiles/village.dir/src/Village/scene.cpp.o" "gcc" "CMakeFiles/village.dir/src/Village/scene.cpp.o.d"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/src/Village/village.cpp" "CMakeFiles/village.dir/src/Village/village.cpp.o" "gcc" "CMakeFiles/village.dir/src/Village/village.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ppgso"
+  "../dependencies/include"
+  "../"
+  "."
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake"
-  "/Users/mariokolenic/Desktop/FIIT/3. semester/Princípy počítačovej grafiky a spracovania obrazu/Semestrálny projekt/PPGSO_projekt/cmake-build-debug/CMakeFiles/shaders.dir/DependInfo.cmake"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake"
+  "D:/Skola/FIIT/3._semester/PPGSO_projekt/cmake-build-debug/CMakeFiles/shaders.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
