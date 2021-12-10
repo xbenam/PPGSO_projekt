29d8243ecf8ccfc14eb9a9a0c63edd8788db845e
xbenam <xbenam@gmail.com>
2021-12-10 12:48:28 +0100

    main changes

--- cmake-build-debug/CMakeFiles/village.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/village.dir/cmake_clean.cmake
@@ -1,15 +1,12 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/village.dir/src/Village/camera.cpp.o"
-  "CMakeFiles/village.dir/src/Village/camera.cpp.o.d"
-  "CMakeFiles/village.dir/src/Village/ground.cpp.o"
-  "CMakeFiles/village.dir/src/Village/ground.cpp.o.d"
-  "CMakeFiles/village.dir/src/Village/object.cpp.o"
-  "CMakeFiles/village.dir/src/Village/object.cpp.o.d"
-  "CMakeFiles/village.dir/src/Village/scene.cpp.o"
-  "CMakeFiles/village.dir/src/Village/scene.cpp.o.d"
-  "CMakeFiles/village.dir/src/Village/village.cpp.o"
-  "CMakeFiles/village.dir/src/Village/village.cpp.o.d"
-  "village"
+  "CMakeFiles/village.dir/src/Village/camera.cpp.obj"
+  "CMakeFiles/village.dir/src/Village/ground.cpp.obj"
+  "CMakeFiles/village.dir/src/Village/object.cpp.obj"
+  "CMakeFiles/village.dir/src/Village/scene.cpp.obj"
+  "CMakeFiles/village.dir/src/Village/village.cpp.obj"
+  "libvillage.dll.a"
+  "village.exe"
+  "village.exe.manifest"
   "village.pdb"
 )
 
--- cmake-build-debug/CMakeFiles/village.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for village.
-# This may be replaced when dependencies are built.
